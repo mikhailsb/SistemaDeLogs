81786441377594da1ee1bfe7b71b3cc6a048ee7d
Mikhail Bispo <anaomsb@hotmail.com>
2022-01-17 00:04:59 -0300

    Iniciando a criacao da classe GravarLog e testes de string

---
+++ SistemaDeLogs/SistemaDeLogs/GravarLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SistemaDeLogs
+{
+    public class GravarLog
+    {
+        public string EnderecoArquivo { get; private set; }
+        public string NomeArquivo { get; private set; }
+        public GravarLog(string caminhoLog, string nomeArq)
+        {
+            EnderecoArquivo = caminhoLog;
+            NomeArquivo = nomeArq;
+        }
+
+        public void Gravar()
+        {
+            if(!File.Exists(EnderecoArquivo+NomeArquivo))
+            {
+
+                Console.WriteLine("Não existe");
+            }
+        }
+
+    }
+}
---
+++ SistemaDeLogs/SistemaDeLogs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SistemaDeLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+
+            string dataArquivo = dt.ToString("dd-MM-yyyy");
+            string nomeArquivo = "mika_log-" + dataArquivo + ".txt";
+
+            GravarLog GL = new GravarLog(@"C:\Mika_log\", nomeArquivo);
+            GL.Gravar();
+
+            Console.WriteLine(nomeArquivo);
+
+            Console.ReadLine();
+        }
+    }
+}
